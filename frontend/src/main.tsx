dad2fb02702a1ff564b26f73dc83d05749095bdf
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-23 18:59:25 -0500

    update chain ID and RPC

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,7 +13,7 @@ import { ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 
 const ethermintChain: Chain = {
-  id: 69420,
+  id: 42000,
   name: 'Ethermint',
   network: 'ethermint',
   nativeCurrency: {
@@ -23,7 +23,7 @@ const ethermintChain: Chain = {
   },
   rpcUrls: {
     default: {
-      http: ['https://a2b5-159-65-252-178.ngrok.io'],
+      http: ['https://d004-159-65-252-178.ngrok.io'],
       // webSocket: ['wss://159.65.252.178:8546/']
     },
   },
